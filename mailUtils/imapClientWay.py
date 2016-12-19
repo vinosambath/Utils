47ba4d89c75258e5d671f59cc75e9bf0113c1663
vinothkumar95 <vinosambath@gmail.com>
2016-12-20 00:36:44 +0530

    Initial commit with two ways of solving problem

---
+++ mailUtils/imapClientWay.py
@@ -0,0 +1,18 @@
+import imapclient, pyzmail
+from datetime import date
+imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
+imapObj.login('vinosambath@gmail.com', 'Vinosam12')
+
+imapObj.select_folder('INBOX', readonly=True)
+UIDs = imapObj.search([u'SINCE', date(2016, 12, 17)])
+print (UIDs)
+
+for uid in UIDs:
+	rawMessages = imapObj.fetch([uid], [b'BODY[]', 'FLAGS'])
+	message = pyzmail.PyzMessage.factory(rawMessages[uid][b'BODY[]'])
+	print(message.get_addresses('from'))
+	print(message.get_addresses('to'))
+	print(message.get_addresses('cc'))
+	print(message.get_addresses('bcc'))
+	#print(message.text_part.get_payload().decode(message.text_part.charset))
+	#print(message.html_part.get_payload().decode(message.html_part.charset))
