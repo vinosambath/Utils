1247c8ec7f4c80500805b9c49f2af1516c5a5e32
vinothkumar95 <vinosambath@gmail.com>
2016-12-24 23:22:58 +0530

    mongo db added

--- mailUtils/imapClientWay.py
+++ mailUtils/imapClientWay.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from datetime import timedelta
+import datetime
 import imapclient, pyzmail
 from datetime import date
 import smtplib
@@ -14,9 +15,7 @@ from celery import Celery
 from celery.schedules import crontab 
 
 from pymongo import MongoClient
-client = MongoClient()
 
-db = client.test
 
 logging.basicConfig()
 #sched = BackgroundScheduler()
@@ -37,76 +36,67 @@ app.conf.update(
 @app.task(name='mail')
 def mail():
 	print("in")
-	imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
-	imapObj.login('fakegithub@gmail.com', 'fakeyou1')
+	client = MongoClient()
 
-	imapObj.select_folder('INBOX', readonly=True)
-	UIDs = imapObj.search([u'SINCE', date(2016, 12, 22)])
+	db = client.test
+	collection = db['mailStat'].find()
+	for item in collection:
+		imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
+		imapObj.login(item["mail"], item["password"])
 
-	fromHash = {}
+		imapObj.select_folder('INBOX', readonly=True)
+		UIDs = imapObj.search([u'SINCE', datetime.datetime.now() - datetime.timedelta(days=1)])
 
-	for uid in UIDs:
-		rawMessages = imapObj.fetch([uid], [b'BODY[]', 'FLAGS'])
-		message = pyzmail.PyzMessage.factory(rawMessages[uid][b'BODY[]'])
-		_from = message.get_addresses('from')
-		_to = message.get_addresses('to')
-		_cc = message.get_addresses('cc')
-		_bcc = message.get_addresses('bcc')
-		_text = None
-		_htmlText = None
-		if(message.text_part != None and message.text_part.charset != None):
-			_text = message.text_part.get_payload().decode(message.text_part.charset)
-		if(message.html_part != None):
-			_htmlText = message.html_part.get_payload().decode(message.html_part.charset)
-		if isinstance(_from, list):
-			#print _from
-			if _from[0][1]  in fromHash:
-				#print "yes"
-				fromHash[_from[0][1]] += 1
+		fromHash = {}
+
+		for uid in UIDs:
+			rawMessages = imapObj.fetch([uid], [b'BODY[]', 'FLAGS'])
+			message = pyzmail.PyzMessage.factory(rawMessages[uid][b'BODY[]'])
+			_from = message.get_addresses('from')
+			_to = message.get_addresses('to')
+			_cc = message.get_addresses('cc')
+			_bcc = message.get_addresses('bcc')
+			_text = None
+			_htmlText = None
+			if(message.text_part != None and message.text_part.charset != None):
+				_text = message.text_part.get_payload().decode(message.text_part.charset)
+			if(message.html_part != None):
+				_htmlText = message.html_part.get_payload().decode(message.html_part.charset)
+			if isinstance(_from, list):
+				#print _from
+				if _from[0][1]  in fromHash:
+					#print "yes"
+					fromHash[_from[0][1]] += 1
+				else:
+					fromHash[_from[0][1]] = 1
 			else:
-				fromHash[_from[0][1]] = 1
-		else:
-			if _from in fromHash:
-				fromHash[_from] += 1
-			else:
-				fromHash[_from] = 1
+				if _from in fromHash:
+					fromHash[_from] += 1
+				else:
+					fromHash[_from] = 1
 
-	MESSAGE = MIMEMultipart('alternative')
-	MESSAGE['subject'] = "mimetext example"
-	MESSAGE['To'] = "fakegithub@gmail.com"
-	MESSAGE['From'] = "fakegithub@gmail.com"
-	MESSAGE.preamble = ""
+		MESSAGE = MIMEMultipart('alternative')
+		MESSAGE['subject'] = "Your Recent Messages"
+		MESSAGE['To'] = item["tomail"]
+		MESSAGE['From'] = item["mail"]
+		MESSAGE.preamble = ""
 
-	s = ""
-	s += "<table>"
-	s += "<tr>"
-	s += "<th> from </th>"
-	s += "<th> Count </th>"
-	s += "</tr>"
-	for key in fromHash:
+		s = ""
+		s += "<table>"
 		s += "<tr>"
-		s += "<th>" + key + "</th>" + "<th>" + str(fromHash[key]) + "</th>"
+		s += "<th> from </th>"
+		s += "<th> Count </th>"
 		s += "</tr>"
+		for key in fromHash:
+			s += "<tr>"
+			s += "<th>" + key + "</th>" + "<th>" + str(fromHash[key]) + "</th>"
+			s += "</tr>"
 
-	HTML_BODY = MIMEText(s, 'html')
-	MESSAGE.attach(HTML_BODY)
-	server = smtplib.SMTP('smtp.gmail.com:587')
+		HTML_BODY = MIMEText(s, 'html')
+		MESSAGE.attach(HTML_BODY)
+		server = smtplib.SMTP('smtp.gmail.com:587')
 
-	server.starttls()
-	server.login("fakegithub@gmail.com", "fakeyou1")
-	server.sendmail("fakegithub@gmail.com", ["fakegithub@gmail.com"], MESSAGE.as_string())
-	server.quit()
-
-	# part2 = MIMEText(s, 'html')
-	# print(smtpObj.sendmail('vinosambath@gmail.com', 'vinosambath@gmail.com', s))
-	# smtpObj.quit()
-# job = sched.add_job(mail, 'interval', minutes=1, id="vinosambath@gmail.com")
-# print job
-
-# # db = client.test
-# # collection = db['mailStat']
-
-
-# # result = db.mailStat.insert_one({"mail":"fakegithub@gmail.com", "password":"fakeyou1", "job": job})
-# # print(result.inserted_id)
-# sched.start()
+		server.starttls()
+		server.login(item["mail"], item["password"])
+		server.sendmail(item["mail"], [item["tomail"]], MESSAGE.as_string())
+		server.quit()
